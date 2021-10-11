82f315511db9f4807a772ead19f58b65953473c7
broda1802 <bebe1802@o2.pl>
2021-10-11 21:20:20 +0200

    login_register

--- Diary/settings.py
+++ Diary/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'patientdiary'
+    'patientdiary',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Diary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [str(BASE_DIR.joinpath('templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
--- patientdiary/views.py
+++ patientdiary/views.py
@@ -4,12 +4,6 @@ from django.views import View
 from patientdiary.models import Patient, Drugs, Doctor, Disease, Substance, Group, Clinic, Pharmacy
 # Create your views here.
 
-class IndexView(View):
-
-    def get(self, request):
-        response = render(request, 'base.html')
-        return response
-
 
 class PatientView(View):
     pass
