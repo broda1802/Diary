b9f78f7454edb49630d299395fb4c233c017460f
broda1802 <bebe1802@o2.pl>
2021-10-12 15:55:11 +0200

    what

--- patientdiary/views.py
+++ patientdiary/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView, ListView
 
+from accounts.models import CustomUser
 from patientdiary.models import Patient, Drugs, Doctor, Disease, Substance, Group, Clinic, Pharmacy
 # Create your views here.
 
@@ -29,6 +31,30 @@ class AddDrugView(View):
 
 class AddDiseaseView(View):
     pass
+
+
+# class ContactsView(DetailView):
+#
+#     def get(self, request):
+#         pharmacies = Pharmacy.objects.all()
+#         clinic = Clinic.objects.all()
+#         doctor = Doctor.objects.all()
+#         return render(request, 'contacts.html',{'pharmacies': pharmacies,
+#                                                 'clinic': clinic,
+#                                                 'doctor': doctor}
+#                       )
+#
+# class ContactsView(ListView):
+#
+#     model = CustomUser
+#     template_name = 'contacts.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['pharmacies'] = Pharmacy.objects.all()
+#         context['clinic'] = Clinic.objects.all()
+#         context['doctor'] = Doctor.objects.all()
+#         return context
 
 
 class DoctorView(View):
