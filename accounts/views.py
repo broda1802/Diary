41b4ef42fc390b90ec790303e99b11c492f9573f
broda1802 <bebe1802@o2.pl>
2021-10-12 10:04:46 +0200

    base_html

--- accounts/views.py
+++ accounts/views.py
@@ -8,5 +8,5 @@ from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('index_view')
     template_name = 'registration/signup.html'
--- patientdiary/views.py
+++ patientdiary/views.py
@@ -4,6 +4,12 @@ from django.views import View
 from patientdiary.models import Patient, Drugs, Doctor, Disease, Substance, Group, Clinic, Pharmacy
 # Create your views here.
 
+class IndexView(View):
+
+    def get(self, request):
+        response = render(request, 'base.html', )
+        return response
+
 
 class PatientView(View):
     pass
