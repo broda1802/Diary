c479c3183daefbf106513bf4086a0632186534d0
broda1802 <bebe1802@o2.pl>
2021-10-18 14:00:18 +0200

    ALOT

--- patientdiary/conftest.py
+++ patientdiary/conftest.py
@@ -1,10 +1,93 @@
 import pytest
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import User, Permission
 from accounts.models import CustomUser
-from patientdiary.models import Patient, Drugs, Disease
+from patientdiary.models import Patient, Drugs, Disease, Substance, Group, Clinic, Pharmacy, Doctor
+from faker import Faker
+
+faker = Faker("pl_PL")
 
 
 @pytest.fixture
 def user():
-    return CustomUser.objects.create(username='abc', password='xyz')
+    return CustomUser.objects.create(username='abc', password='laseczek1')
+
+
+@pytest.fixture
+def patient(disease, drug, clinic, user):
+    lst = []
+    for p in range(10):
+        p = Patient.objects.create(first_name=faker.text(max_nb_chars=20), last_name=faker.text(max_nb_chars=20),
+                                   Pesel=1234, phone=12345, my_history=faker.text(max_nb_chars=20),
+                                   clinic=clinic[0], user=user)
+        p.disease.set(disease)
+        p.drug.set(drug)
+        lst.append(p)
+
+    return lst
+
+
+@pytest.fixture
+def disease():
+    lst = []
+    for p in range(10):
+        lst.append(Disease.objects.create(name=faker.text(max_nb_chars=20), description=faker.text(max_nb_chars=20),
+                                          ))
+    return lst
+
+
+@pytest.fixture
+def drug(substance):
+    lst = []
+    for p in range(10):
+        p = Drugs.objects.create(name=faker.text(max_nb_chars=20), leaflet=faker.text(max_nb_chars=20),
+                                 dosage=1, action=faker.text(max_nb_chars=20),
+                                 )
+        p.substances.add(substance[0])
+        lst.append(p)
+    return lst
+
+
+@pytest.fixture
+def substance():
+    lst = []
+    for p in range(10):
+        lst.append(Substance.objects.create(substance_name=faker.text(max_nb_chars=20)))
+    return lst
+
+
+@pytest.fixture
+def group():
+    lst = []
+    for p in range(10):
+        lst.append(Group.objects.create(symbol=faker.text(max_nb_chars=20),
+                                        name=faker.text(max_nb_chars=20)))
+    return lst
+
+
+@pytest.fixture
+def clinic():
+    lst = []
+    for p in range(10):
+        lst.append(Clinic.objects.create(name=faker.text(max_nb_chars=20), city=faker.text(max_nb_chars=20),
+                                         street=faker.text(max_nb_chars=20), phone=1))
+    return lst
+
+
+@pytest.fixture
+def pharmacy():
+    lst = []
+    for p in range(10):
+        lst.append(Pharmacy.objects.create(name=faker.text(max_nb_chars=20), city=faker.text(max_nb_chars=20),
+                                           street=faker.text(max_nb_chars=20), phone=1, opening_hours=1))
+    return lst
+
+
+@pytest.fixture
+def doctor():
+    lst = []
+    for p in range(10):
+        lst.append(Doctor.objects.create(first_name=faker.text(max_nb_chars=20), last_name=faker.text(max_nb_chars=20),
+                                         medical_specialization=faker.text(max_nb_chars=20)))
+    return lst
+
+
--- patientdiary/tests.py
+++ patientdiary/tests.py
@@ -62,3 +62,117 @@ def test_login_post(user):
     assert response.status_code == 302
 
 
+@pytest.mark.django_db
+def test_drug_list_view_no_login():
+    client = Client()
+    response = client.get(reverse('drugs_view'))
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_drug_list_view_logged(user):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('drugs_view'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_drug_delete_logged(user, drug):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('drug_delete_view', kwargs={'pk': drug[0].pk}))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_drug_delete_logged(user, drug):
+    client = Client()
+    client.force_login(user)
+    response = client.post(reverse('drug_delete_view', kwargs={'pk': drug[0].pk}))
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_drug_create_logged(user):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('drug_add_view'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_drug_create_logged(user, substance):
+    client = Client()
+    client.force_login(user)
+    a = {
+        'name': 'Nazwa',
+        'leaflet': 'Ulotka',
+        'dosage': '1',
+        'action': 'Działanie',
+        'substances': substance[0].pk
+    }
+    response = client.post(reverse('drug_add_view'), data=a)
+    assert response.status_code == 302
+
+
+
+
+
+
+
+
+
+
+
+
+@pytest.mark.django_db
+def test_disease_list_view_no_login():
+    client = Client()
+    response = client.get(reverse('diseases_view'))
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_disease_list_view_logged(user):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('diseases_view'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_disease_delete_logged(user, disease):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('disease_delete_view', kwargs={'pk': disease[0].pk}))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_disease_delete_logged(user, disease):
+    client = Client()
+    client.force_login(user)
+    response = client.post(reverse('disease_delete_view', kwargs={'pk': disease[0].pk}))
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_disease_create_logged(user):
+    client = Client()
+    client.force_login(user)
+    response = client.get(reverse('disease_add_view'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_disease_create_logged(user):
+    client = Client()
+    client.force_login(user)
+    a = {
+        'name': 'Nazwa',
+        'description': 'Opis'
+    }
+    response = client.post(reverse('disease_add_view'), data=a)
+    assert response.status_code == 302
+
